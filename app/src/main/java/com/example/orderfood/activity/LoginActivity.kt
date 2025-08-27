44220560f4c0c370f164df312f10b72646904a89
Dinh Quoc Viet <vietdinh083@gmail.com>
2025-08-27 11:51:43 +0900

    fix btnLogin khong cho user an nhieu lan

--- app/src/main/java/com/example/orderfood/activity/LoginActivity.kt
+++ app/src/main/java/com/example/orderfood/activity/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.orderfood.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -19,10 +21,10 @@ import com.google.firebase.auth.FirebaseAuth
 import io.paperdb.Paper
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var edtEmail:EditText
-    private lateinit var  edtPassword:EditText
-    private lateinit var btnLogin : Button
-    private lateinit var btnRegister : Button
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
     private lateinit var progressBar: ProgressBar
     private lateinit var mAuth: FirebaseAuth
     private lateinit var checkBox: CheckBox
@@ -39,12 +41,17 @@ class LoginActivity : AppCompatActivity() {
         buttonAndTextEvents()
 
 
-
     }
 
     private fun buttonAndTextEvents() {
         btnLogin.setOnClickListener {
             loginUserAccount()
+            btnLogin.isEnabled = false
+            Handler(Looper.getMainLooper()).postDelayed({
+                btnLogin.isEnabled = true
+                progressBar.visibility = View.GONE
+            }, 3000)
+
         }
         btnRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
@@ -76,15 +83,17 @@ class LoginActivity : AppCompatActivity() {
             mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     // login successful
-
-                   //Log.d("TAG", "loginUserAccount: ${mAuth.currentUser?.uid}")
-                    Utils.current_User = UserModel(mAuth.currentUser?.uid.toString(),email,password)
+                    //Log.d("TAG", "loginUserAccount: ${mAuth.currentUser?.uid}")
+                    Utils.current_User =
+                        UserModel(mAuth.currentUser?.uid.toString(), email, password)
                     progressBar.visibility = View.VISIBLE
-                    Toast.makeText(this, "Login successful!!", Toast.LENGTH_LONG).show()
+
                     startActivity(Intent(this, MainActivity::class.java))
+                    Toast.makeText(this, "Login successful!!", Toast.LENGTH_LONG).show()
+
                 } else {
                     // login failed
-                    Log.e("Error",task.exception.toString())
+                    Log.e("Error", task.exception.toString())
                     Toast.makeText(this, "Login failed!!", Toast.LENGTH_LONG).show()
                 }
 
